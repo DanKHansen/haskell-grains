b4a127d8d30ef6347339eccfb07e78ca07d096fb
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-14 09:23:48 +0200

    solution

--- src/Grains.hs
+++ src/Grains.hs
@@ -1,7 +1,11 @@
 module Grains (square, total) where
 
+import Data.Bits (Bits (shiftL))
+
 square :: Integer -> Maybe Integer
-square n = error "You need to implement this function."
+square n
+  | n >= 1 && n < 65 = Just (shiftL 1 (fromIntegral (n - 1)))
+  | otherwise = Nothing
 
 total :: Integer
-total = error "You need to implement this function."
+total = 1 `shiftL` 64 - 1
